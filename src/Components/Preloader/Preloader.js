ce9e0bf0a2d799115bdeb9baf3040041f67c3cef
Jeandre Melaria <j.melaria@hotmail.com>
2018-08-14 16:41:22 +0200

    Add:Media querie styles to preloader(min-width:1707px)

--- src/Components/Preloader/Preloader.js
+++ src/Components/Preloader/Preloader.js
@@ -3,7 +3,7 @@ import React from 'react';
 const preloader = () => {
   return (
           <div className = 'preloader'> 
-            <span className = 'preloaderCircle preloaderCircle--purple'></span>
+            <div className = 'preloaderCircle preloaderCircle--purple'></div>
             <div className = 'preloaderCircle preloaderCircle--white'></div>
             <div className = 'preloaderCircle preloaderCircle--purple'></div>
           </div>
