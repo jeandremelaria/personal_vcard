a5d866fdb7743b945d1923280f8e6efe0cb57aac
Jeandre Melaria <j.melaria@hotmail.com>
2018-08-15 23:58:25 +0200

    Add: react-materialize Row and Col to preloader

--- src/Components/Preloader/Preloader.js
+++ src/Components/Preloader/Preloader.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import TweenMax from 'gsap';
+import { Row, Col } from 'react-materialize';
 
-const preloader = () => {
-  return (
-          <div className = 'preloader'> 
-            <div className = 'preloaderCircle preloaderCircle--purple'></div>
-            <div className = 'preloaderCircle preloaderCircle--white'></div>
-            <div className = 'preloaderCircle preloaderCircle--purple'></div>
-          </div>
-  );
+class Preloader extends Component {
+  componentDidMount() {
+  TweenMax.staggerFrom('.preloaderCircle', 2, {scale:-1.2, repeat:-1, }, 0.5);
+ }
+  render() {
+    return (
+      <Row className = 'preloader'>
+        <Col s={1} className = 'preloaderCircle preloaderCircle--purple'></Col>
+        <Col s={1} className = 'preloaderCircle preloaderCircle--white'></Col>
+        <Col s={1} className = 'preloaderCircle preloaderCircle--purple'></Col>
+      </Row>
+    );
+  }
 }
 
-export default preloader;
+export default Preloader;
