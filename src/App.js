0478b3da96cb477c5a03cb4bb910100645120fc0
Jeandre Melaria <j.melaria@hotmail.com>
2018-08-21 10:26:21 +0200

    Add: Preloader component and logic to App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import './App.css';
 import Preloader from './Components/Preloader/Preloader';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = { isLoading: true }
+  }
+  
+  componentDidMount(){
+    // Set isLoading to false after DOM is rendered
+    // this.setState({ isLoading: false}, ()=>{
+    //   console.log(this.state.isLoading);
+    // }); 
+    this.setState({ isLoading: false });
+  }
+
   render() {
-    return (
-      <div className="App">
-          <Preloader/>
-      </div>
-    );
+    const isLoading = this.state.isLoading;
+    return ( isLoading ? <Preloader/> : <p>Test</p> );
   }
 }
 
